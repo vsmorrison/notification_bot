996db2da4ef564781462f8b5fec0b01d04d10ce8
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-01-13 01:33:00 +0300

    Replace odd code into new file

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-123123
+import os
+from dotenv import load_dotenv
+from long_polling import get_reviews
+
+
+def main():
+    load_dotenv()
+    dvmn_token = os.getenv('dvmn_token')
+    tg_token = os.getenv('tg_token')
+    url = 'https://dvmn.org/api/long_polling/'
+    response = get_reviews(url, dvmn_token)
+
+
+if __name__ == "__main__":
+    main()
+
+
