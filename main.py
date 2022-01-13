18ea838011340233f1a006a6b4d1170febe32bfb
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-01-13 17:10:10 +0300

    Add arguments for invocation

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import os
 from dotenv import load_dotenv
-from long_polling import get_reviews
+import long_polling
 
 
 def main():
     load_dotenv()
     dvmn_token = os.getenv('dvmn_token')
     tg_token = os.getenv('tg_token')
+    chat_id = os.getenv('chat_id')
     url = 'https://dvmn.org/api/long_polling/'
-    response = get_reviews(url, dvmn_token)
+    long_polling.get_server_response(
+        url, dvmn_token, tg_token, chat_id, timestamp=None
+    )
 
 
 if __name__ == "__main__":
     main()
-
-
