d26fe98dae2f3c59d9398ca973d80d9919f5ee0e
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-01-19 23:04:31 +0300

    Add creating instance of tg bot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import long_polling
+import telegram
 
 
 def main():
@@ -9,9 +10,9 @@ def main():
     tg_token = os.getenv('tg_token')
     chat_id = os.getenv('chat_id')
     url = 'https://dvmn.org/api/long_polling/'
-    long_polling.get_server_response(
-        url, dvmn_token, tg_token, chat_id, timestamp=None
-    )
+    bot = telegram.Bot(token=tg_token)
+    long_polling.make_server_polling(
+        url, dvmn_token, chat_id, bot, timestamp=None)
 
 
 if __name__ == "__main__":
