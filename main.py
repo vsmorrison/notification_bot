d04b36be78ac4f1c03f1748a7cb75d72962053f2
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-03-23 00:33:42 +0300

    Add logger call

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 import long_polling
 import telegram
+import logging_tools
 
 
 def main():
@@ -11,8 +12,9 @@ def main():
     chat_id = os.getenv('CHAT_ID')
     url = 'https://dvmn.org/api/long_polling/'
     bot = telegram.Bot(token=tg_token)
+    logger = logging_tools.make_bot_logger(bot, chat_id)
     long_polling.make_server_polling(
-        url, dvmn_token, chat_id, bot, timestamp=None)
+        url, dvmn_token, chat_id, bot, logger, timestamp=None)
 
 
 if __name__ == "__main__":
