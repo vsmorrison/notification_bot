04482ab5e4b3b15e8984f12cad3ee2f8e1e0d666
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-03-23 00:53:31 +0300

    Fix naming

--- logging_tools.py
+++ logging_tools.py
@@ -3,14 +3,16 @@ import logging
 
 class TelegramBotHandler(logging.Handler):
 
-    def __init__(self, tg_bot, chat_id):
+    def __init__(self, tg_logging_bot, tg_chat_id):
         super().__init__()
-        self.chat_id = chat_id
-        self.tg_bot = tg_bot
+        self.tg_chat_id = tg_chat_id
+        self.tg_logging_bot = tg_logging_bot
 
     def emit(self, record):
         log_entry = self.format(record)
-        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+        self.tg_logging_bot.send_message(
+            chat_id=self.tg_chat_id, text=log_entry
+        )
 
 
 def make_bot_logger(bot, chat_id):
--- main.py
+++ main.py
@@ -9,12 +9,12 @@ def main():
     load_dotenv()
     dvmn_token = os.getenv('DVMN_TOKEN')
     tg_token = os.getenv('TG_TOKEN')
-    chat_id = os.getenv('CHAT_ID')
+    tg_chat_id = os.getenv('TG_CHAT_ID')
     url = 'https://dvmn.org/api/long_polling/'
     bot = telegram.Bot(token=tg_token)
-    logger = logging_tools.make_bot_logger(bot, chat_id)
+    logger = logging_tools.make_bot_logger(bot, tg_chat_id)
     long_polling.make_server_polling(
-        url, dvmn_token, chat_id, bot, logger, timestamp=None)
+        url, dvmn_token, tg_chat_id, bot, logger, timestamp=None)
 
 
 if __name__ == "__main__":
